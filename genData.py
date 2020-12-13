8df159d978025810e0a53fdb57d6935576a802f1
joosu77 <joosep.naks@gmail.com>
2020-12-13 14:33:43 +0200

    Changed fitness function to only be based on the final points of the player

--- genData.py
+++ genData.py
@@ -84,8 +84,8 @@ def generate(pathPrefix, model_file="", model=None, p1=None, p2=None, p3=None):
             over = g.resolve_workers()
         print (f"Scores: {g.points}", end="\t")
         
-        avgpoints = sum(g.points)/3
-        fit = [i-avgpoints for i in g.points]
+        #avgpoints = sum(g.points)/3
+        fit = [i for i in g.points]
         
         mems = itertools.chain.from_iterable([p.getMem() for p in players])
 
